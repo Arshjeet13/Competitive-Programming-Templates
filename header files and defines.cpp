cec3bcff8aca0822f324c7737d677b2110c02dd1
Arshjeet Singh Bajwa <arshjeet.bajwa@gmail.com>
2026-01-30 14:45:03 -0600

    Added header files template and comments in binary exponenation

--- header files and defines.cpp
+++ header files and defines.cpp
@@ -9,23 +9,10 @@ using namespace __gnu_pbds;
 #define fast() ios::sync_with_stdio(false); cin.tie(NULL)
 typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
 
-const int MOD = 998244353;
- 
-/*
-Returns a^b modulo MOD
-*/
-int exp(int a, int b) {
-    if (b == 0) return 1;
-    if (b ==  1) return a;
-    int half = exp(a, b/2) % MOD;
-    int ans = (half * half) % MOD;
-    if (b&1) {ans *= a; ans %= MOD;}
-    return ans;
-}
- 
-signed main(){
+
+signed main()
     fast();
- 
+
     //int tc = 1;
     int tc; cin>>tc;
     while(tc--){
