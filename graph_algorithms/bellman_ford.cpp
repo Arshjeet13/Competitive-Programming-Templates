2dc3cfdfc77fae9421e18482049fde22e9c217b7
Arshjeet Singh Bajwa <arshjeet.bajwa@gmail.com>
2026-05-23 19:29:20 +0530

    Add bellman ford code

---
+++ graph_algorithms/bellman_ford.cpp
@@ -0,0 +1,31 @@
+int INF = 1e15+3;
+vector<vector<int>> edges;
+
+for(int i=0; i<n-1; ++i){
+    for(auto edge : edges){
+        int u = edge[0];
+        int v = edge[1];
+        int w = edge[2];
+
+        if(dist[u] != INF){
+            dist[v] = min(dist[v], dist[u] + w);
+        }
+    }
+}
+
+// After running one more iteration of the bellman ford, atleast one node from
+// every single negative cycle (reachable from the source) must be present in vec
+// Moreover, it may also contain nodes not belonging to any negative cycle but
+// reachable from some negative cycle(s)
+vector<int> vec;
+for(auto edge : edges){
+    int u = edge[0];
+    int v = edge[1];
+    int w = edge[2];
+
+    if(dist[u] != INF){
+        if(dist[v] > dist[u] + w){
+            vec.pb(v);
+        }
+    }
+}
