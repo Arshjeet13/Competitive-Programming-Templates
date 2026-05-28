4496577655fb1dc3f487c5bb4dbee389ea964332
Arshjeet Singh Bajwa <arshjeet.bajwa@gmail.com>
2026-05-28 13:27:40 +0530

    Add initialization for array d in floyd warshall

--- graph_algorithms/floyd_warshall.cpp
+++ graph_algorithms/floyd_warshall.cpp
@@ -1,6 +1,15 @@
 const int N = 100;
 const int INF = 1e18+3;
 int d[N][N];
+int adj[N][N];
+
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+        if (i == j) d[i][j] = 0;
+        else if (adj[i][j]) d[i][j] = adj[i][j];
+        else d[i][j] = INF;
+    }
+}
 
 for(int k=1; k<=n; ++k){
     for(int i=1; i<=n; ++i){
