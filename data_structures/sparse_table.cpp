1ed8b57dd6f16072339c868e14a7ff92bc86d752
Arshjeet Singh Bajwa <arshjeet.bajwa@gmail.com>
2026-06-22 23:19:26 +0530

    Add sparse table code

---
+++ data_structures/sparse_table.cpp
@@ -0,0 +1,20 @@
+const int N = 2e5+3;
+int sparse[N][21];
+int a[N];
+
+void build(int n){
+    for(int i=0; i<n; ++i){
+        sparse[i][0] = a[i];
+    }
+
+    for(int j=1; j<21; ++j){
+        for(int i=0; i + (1<<j) - 1 < n; ++i){
+            sparse[i][j] = min(sparse[i][j-1], sparse[i + (1<<(j-1))][j-1]);
+        }
+    }
+}
+
+int query(int l, int r){
+    int log = (int) log2(r-l+1);
+    return min(sparse[l][log], sparse[r-(1<<log)+1][log]);
+}
