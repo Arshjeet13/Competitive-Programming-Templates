9659b26d13a6c317716e8d0c4c3decdbb4c0952e
Arshjeet Singh Bajwa <arshjeet.bajwa@gmail.com>
2026-06-25 23:34:54 +0530

    Edited log value to be dynamic based of choice of N

--- data_structures/sparse_table.cpp
+++ data_structures/sparse_table.cpp
@@ -1,13 +1,14 @@
 const int N = 2e5+3;
-int sparse[N][21];
+const int K = (int)log2(N) + 1;
+int sparse[N][K];
 int a[N];
 
 void build(int n){
-    for(int i=0; i<n; ++i){
+    for(int i=0; i<K; ++i){
         sparse[i][0] = a[i];
     }
 
-    for(int j=1; j<21; ++j){
+    for(int j=1; j<20; ++j){
         for(int i=0; i + (1<<j) - 1 < n; ++i){
             sparse[i][j] = min(sparse[i][j-1], sparse[i + (1<<(j-1))][j-1]);
         }
@@ -15,6 +16,6 @@ void build(int n){
 }
 
 int query(int l, int r){
-    int log = (int) log2(r-l+1);
-    return min(sparse[l][log], sparse[r-(1<<log)+1][log]);
+    int log = (int)log2(r-l+1);
+    return min(sparse[l][log], sparse[r - (1<<log) + 1][log]);
 }
